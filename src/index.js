fa82918eb48d5d3c7d7f6fa55ea325f51a71ae53
Kevin Huang <yellow.khaos@gmail.com>
2019-02-06 13:34:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ const Home = () => {
   );
 };
 
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <Route path = "/" render={Home} />
+  </Router>),
   document.getElementById('root')
 );
