11f3bccb0b3189ab351d381ac014149f12a63202
Kevin Huang <yellow.khaos@gmail.com>
2019-02-06 13:54:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-
+import Home from './Home'
+import About from './About'
+import Login from './Login'
 
 ReactDOM.render((
   <Router>
