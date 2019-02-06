b2f4c45fef60f074aec474333f10293a2c771275
Kevin Huang <yellow.khaos@gmail.com>
2019-02-06 13:39:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -11,6 +11,34 @@ const Home = () => {
   );
 };
 
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  )
+}
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+
+          <label htmlFor="username">Username</label>
+        </div>
+          <input type="password" name="password" placeholder="Password" />
+
+          <label htmlFor="password">Password</label>
+        <div>
+
+        </div>
+      </form>
+    </div>
+  )
+}
+
 ReactDOM.render((
   <Router>
     <Route path = "/" render={Home} />
