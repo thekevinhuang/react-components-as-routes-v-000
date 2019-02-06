2bb1afda6673993b4531651ad1cd67ca99deb5ad
Kevin Huang <yellow.khaos@gmail.com>
2019-02-06 14:03:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
 
 import Home from './Home'
 import About from './About'
 import Login from './Login'
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar
+
 ReactDOM.render((
   <Router>
     <>
+      <Navbar />
       <Route exact path = "/" component={Home} />
       <Route exact path= "/about" component={About} />
       <Route exact path= "/login" component={Login} />
